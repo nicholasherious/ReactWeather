a112dcd89538f37c9e4fa8e0e48dbaeaee139d31
nicholasherious <nherious@gmail.com>
2017-04-15 19:44:29 -0400

    Prep repo for Heroku

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var express = require('express');
 //Create our app
 
 var app = express();
-const port = process.env.PORT; || 3000;
+const port = process.env.PORT || 3000;
 
 app.use(function(req, res, next){
   if (req.headers['x-forwarded-proto'] === 'http') {
