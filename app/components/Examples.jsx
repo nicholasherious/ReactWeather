2740a15df1f7aad187178146152ad2d36257169a
nicholasherious <nherious@gmail.com>
2017-04-15 19:03:48 -0400

    Add readme file

--- app/components/Examples.jsx
+++ app/components/Examples.jsx
@@ -2,7 +2,10 @@ const React = require('react');
 
 const Examples = (props) => {
     return (
+        <div>
         <h2>Examples!</h2>
+            <p>Welcome to examples page</p>
+        </div>
     )
 };
 
