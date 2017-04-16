59efcefe3d9012b0f1227696165e8e3f2a7c7eb2
nicholasherious <nherious@gmail.com>
2017-04-15 21:27:56 -0400

    style nav bar

--- app/components/Examples.jsx
+++ app/components/Examples.jsx
@@ -3,8 +3,23 @@ const React = require('react');
 const Examples = (props) => {
     return (
         <div>
-        <h2>Examples</h2>
-            <p>Welcome to examples page</p>
+        <h1 className="text-center">Examples</h1>
+            <p>
+                This is a weather application built with React using the Complete React Web App developer course.
+            </p>
+            <p>
+                Here are some of the tools I used
+            </p>
+            <p>
+                <ul>
+                    <li>
+                        <a href="https://facebook.github.io/react">React</a> Javascript framework
+                    </li>
+                    <li>
+                        <a href="http://openweathermap.org/">OpenWeatherMap API</a>
+                    </li>
+                </ul>
+            </p>
         </div>
     )
 };
